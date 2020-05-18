3c6fb524cb519ee5bade04ea4282e13615504abd
Jean Ouina <jeanouina@gmail.com>
2020-05-18 22:02:27 +0200

    fix des node_modules non installés quand on les prends depuis github

---
+++ installSubModules.js
@@ -0,0 +1,31 @@
+const child_process = require("child_process")
+//const fs = require("fs")
+const path = require("path")
+/*
+const MODULES_DIRNAME = path.join(__dirname, "modules")
+
+fs.readdirSync(MODULES_DIRNAME, {withFileTypes: true})
+.forEach(e => {
+    if(!e.isDirectory())return
+    const MODULE_DIRNAME = path.join(MODULES_DIRNAME, e.name)
+    if(!fs.existsSync(path.join(MODULE_DIRNAME, "package.json")))return
+    if(e.name === "discord_spellcheck")return
+    
+    console.log(`Installing modules in ${e.name}.`)
+
+    child_process.exec("npm i", {
+        cwd: MODULE_DIRNAME,
+        env: process.env
+    }, () => {})
+})*/
+
+
+const MODULE_DIRNAME = path.join(__dirname, "modules", "discord_desktop_core", "core")
+child_process.spawn((process.platform === "win32" ? "npm.cmd" : "npm"), ["i"], {
+    cwd: MODULE_DIRNAME,
+    env: process.env,
+    stdio: "inherit"
+}).on("error", (err) => {
+    console.error(err)
+    process.exit(1)
+})

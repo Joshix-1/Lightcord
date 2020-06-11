7f557ca857d4f02d172d345237f334288472cd79
Jean Ouina <jeanouina@gmail.com>
2020-06-11 13:28:51 +0200

    npm audit fix + misc

--- BetterDiscordApp/minify.js
+++
@@ -1,8 +0,0 @@
-const terser = require("terser")
-const fs = require("fs")
-
-fs.writeFileSync(__dirname+"/js/main.min.js", terser.minify(fs.readFileSync(__dirname+"/js/main.js", "utf8"), {
-    "output": {
-        "comments": false
-    }
-}).code)

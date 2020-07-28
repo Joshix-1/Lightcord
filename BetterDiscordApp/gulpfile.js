588a0f6f3a66e9c7508631ca204918541853eeb9
Jean Ouina <jeanouina@gmail.com>
2020-07-28 03:23:27 +0200

    Sync with rauenzi/BetterDiscordApp a84efe7 rework before upgrade

--- BetterDiscordApp/gulpfile.js
+++ BetterDiscordApp/gulpfile.js
@@ -1,33 +1,15 @@
 const gulp = require("gulp");
 const rename = require("gulp-rename");
-const minify = require("gulp-babel-minify");
 const csso = require("gulp-csso");
 
-gulp.task("minify-js", minifyJS);
 gulp.task("minify-css", minifyCSS);
-
-gulp.task("watch-js", function() {
-  return gulp.watch(["./dist/index.js"], minifyJS);
-});
-
 gulp.task("watch-css", function() {
-  return gulp.watch(["./dist/style.css"], minifyCSS);
+  return gulp.watch(["./src/styles/index.css"], minifyCSS);
 });
-
-gulp.task("watch", function() {
-  return gulp.watch(["./dist/index.js", "./dist/style.css"], gulp.series(minifyJS, minifyCSS));
-});
-
-function minifyJS() {
-  return gulp.src("./dist/index.js")
-  .pipe(minify({mangle: {keepClassName: true}}))
-  .pipe(rename("index.min.js"))
-  .pipe(gulp.dest("./dist"));
-}
 
 function minifyCSS() {
-  return gulp.src("./dist/style.css")
+  return gulp.src("./src/styles/index.css")
         .pipe(csso({restructure: false}))
-        .pipe(rename("style.min.css"))
+        .pipe(rename("style.css"))
         .pipe(gulp.dest("./dist"));
 }
